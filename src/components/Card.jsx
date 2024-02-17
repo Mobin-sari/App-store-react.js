d554494d6c470f718486ee7bc548e2a12fa806c5
Mobin Sari <mobinsarijs@gmail.com>
2024-02-16 20:37:08 -0800

    create search box

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,15 +3,18 @@ import { Link } from "react-router-dom";
 import { CgDetailsMore } from "react-icons/cg";
 import { TbShoppingBagCheck } from "react-icons/tb";
 import { shortenText } from "../helper/helper";
+
+import styles from "../styles/card.module.css"
+
 function Card({ data }) {
   const { id, title, image, price } = data;
 
   return (
-    <div>
+    <div className={styles.card}>
       <img src={image} alt={title} />
       <h3>{shortenText(title)}</h3>
       <p>{price}</p>
-      <div>
+      <div className={styles.actions}>
         <Link to={`/product/${id}`}>
           <CgDetailsMore />
         </Link>
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,21 +1,80 @@
+import { ImSearch } from "react-icons/im";
+import { FaListUl } from "react-icons/fa";
+
 import Card from "../components/Card";
+import Loader from "../components/Loader";
 import { useProducts } from "../context/ProductContext";
 
-// import styles from "../styles/productPage.module.css";
+import { filterProducts, searchProducts } from "../helper/helper";
+import styles from "../styles/productPage.module.css";
+import { useEffect, useState } from "react";
 
 function ProductsPage() {
   const products = useProducts();
+
+  const [displayed, setDisplayed] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState({});
+
+  useEffect(() => {
+    setDisplayed(products);
+  }, [products]);
+
+  useEffect(() => {
+    let finalProducts = searchProducts(products, query.search);
+    filterProducts(finalProducts, query.category);
+    setDisplayed(finalProducts);
+  }, [query]);
+
+  const searchHandler = () => {
+    setQuery((query) => ({ ...query, search }));
+  };
+
+  const categorHandler = (event) => {
+    const { tagName } = event.target;
+    const category = event.target.innerText.toLowerCase();
+    if (tagName !== "LI") return;
+    setQuery((query) => ({ ...query, category }));
+  };
+
   return (
-    <div>
+    <>
       <div>
-        {!products.length && <p>loading ...</p>}
-        pages
-        {products.map((p) => (
-          <Card key={p.id} data={p} />
-        ))}
+        <input
+          type="text"
+          placeholder="Search ..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLocaleLowerCase().trim())}
+        />
+        <button onClick={searchHandler}>
+          <ImSearch />
+        </button>
       </div>
-      <div>sidebar</div>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!products.length && (
+            <>
+              <Loader />
+            </>
+          )}
+          {products.map((p) => (
+            <Card key={p.id} data={p} />
+          ))}
+        </div>
+        <div></div>
+        <div>
+          <FaListUl />
+          <p>Categories</p>
+        </div>
+        <ul onClick={categorHandler}>
+          <li>All</li>
+          <li>Electronics</li>
+          <li>Jewelery</li>
+          <li>Men's Clothing</li>
+          <li>Women's Clothing</li>
+        </ul>
+      </div>
+    </>
   );
 }
 
